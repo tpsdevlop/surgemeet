0e50509f9e00be8bb523c0ee0d5cbfb0c91ce479
Coview2001 <kecoview@gmail.com>
2024-10-03 18:22:00 +0530

    allmightypush0211024v8

--- web_app/views.py
+++ web_app/views.py
@@ -4,4 +4,3 @@ from django.shortcuts import render
 # Create your views here.
 def home (request):
     return HttpResponse('Welcome to the good home page push push 031024v5')
->>>>>>> 8f29d64e79b35791c139edaeda6c2c4bc938caab
