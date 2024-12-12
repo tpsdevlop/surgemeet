fb42d3396f8df0b2d668f68cd79b069cc400fc33
tpsdevlop <developers@thoughtprocess.co.in>
2024-12-12 12:33:44 +0530

    12-12-24-1

--- web_app/views.py
+++ web_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from datetime import datetime, timedelta
+ONTIME = datetime.utcnow().__add__(timedelta(hours=5, minutes=30))
 # Create your views here.
 def home (request):
-    return HttpResponse('Welcome to the good home page push 02-12-24')
+    return HttpResponse('Welcome to the good home page push :'+str(ONTIME))
